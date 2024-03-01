8441d134509781d81aab6366f98fa25168b2e203
Valeryio <tommagoma23@gmail.com>
2024-03-01 22:32:00 +0100

    First reset of the repo
    I've just reset all the data on the repository to allow a new code
    to be deployed!

--- public/js/javas.js
+++
@@ -1,9 +0,0 @@
-function showSecondForm() {
-    document.getElementById('first-form').style.display = 'none';
-    document.getElementById('second-form').style.display = 'block';
-}
-
-function showFirstForm() {
-    document.getElementById('second-form').style.display = 'none';
-    document.getElementById('first-form').style.display = 'block';
-}
